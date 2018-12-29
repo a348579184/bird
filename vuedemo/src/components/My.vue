a8c2a30155e5922b657eef7c21bead8a90367487
chenye <chenye@qq.com>
2018-12-29 10:40:42 +0800

    1

--- vuedemo/src/components/My.vue
+++ vuedemo/src/components/My.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-  	<p>123</p>
+  	<div class="header">
+  		我
+    </div> 
+    <div class="png"></div>
   </div>
 </template>
 
@@ -17,5 +20,22 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.header{
+  	height:56px;
+  	background: linear-gradient(to left,#22b9ff,#0a9ee8);
+  	font-size: 24px;
+  	width: 100%;
+  	line-height: 56px;
+  	text-align: center;
+  	color:white;
+  	border-radius: 4px;
+  	position: absolute;
+  	top:0px;
+  }
+  .png{
+  	margin-top:56px;
+  	height:160px;
+  	background-color: #E8EAED;
+  	width:100%;
+  }
 </style>
--- vuedemo/src/components/faxian.vue
+++ vuedemo/src/components/faxian.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-  	faxian
-  </div>
+  	<div class="header">
+      发现
+    </div>  </div>
 </template>
 
 <script>
@@ -16,5 +17,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.header{
+  	height:56px;
+  	background: linear-gradient(to left,#22b9ff,#0a9ee8);
+  	font-size: 24px;
+  	width: 100%;
+  	line-height: 56px;
+  	text-align: center;
+  	color:white;
+  	border-radius: 4px;
+  	position: absolute;
+  	top:0px;
+  }
 </style>
